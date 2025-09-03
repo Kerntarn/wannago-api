36316723297c7a203f25fbfd48b7e37535c970d7
nickytk <nccky.ades@gmail.com>
2025-09-03 22:36:41 +0700

    Add decorator Match

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,19 @@
+import { IsNotEmpty, MinLength } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Match } from '../decorators/match.decorator';
+
+export class RegisterDto {
+    @ApiProperty()
+    @IsNotEmpty()
+    name: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @MinLength(8)
+    password: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @Match('password', { message: 'Passwords do not match' })
+    confirmPassword: string;
+}
