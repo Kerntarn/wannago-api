ae82cf3d78be185cbd7e1b4f9f992d1c7da1d3a2
JuniorMJMJ <manchaibooncha@gmail.com>
2025-09-03 19:28:32 +0700

    create transaction dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { PlacesModule } from './places/places.module';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
     MongooseModule.forRoot(`${process.env.MONGO_URL}`),
-    PlacesModule],
-  controllers: [AppController],
+    PlacesModule,
+    TransactionModule,],
+  controllers: [AppController,],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/dtos/transaction.dto.ts
@@ -0,0 +1,93 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+      IsNotEmpty,
+      IsNumber,
+      IsPositive,
+      IsString,
+      IsIn,
+      IsDateString,
+      IsEnum,
+} from 'class-validator';
+
+export enum TransactionStatus {
+      PENDING = 'pending',
+      ACCEPTED = 'accepted',
+      REJECTED = 'rejected',
+}
+
+export enum PaymentMethod {
+      VISA = 'Visa',
+      MASTERCARD = 'MasterCard',
+      QR = 'QR Payment',
+      PROMPTPAY = 'PromptPay',
+      MOBILE = 'Mobile Banking',
+}
+
+export class CreateTransactionDto {
+
+      // purpose ของ transaction
+      @ApiProperty({
+      description: 'Transaction purpose',
+      example: 'Payment for hotel adds',
+      })
+      @IsString()
+      @IsNotEmpty()
+      purpose!: string;
+
+      // status ของ transaction
+      @ApiProperty({
+      description: 'Transaction status',
+      example: TransactionStatus.PENDING,
+      enum: TransactionStatus,
+      })
+      @IsNotEmpty()
+      @IsEnum(TransactionStatus, {
+      message: 'Status must be either pending, accepted, or rejected',
+      })
+      status!: string;
+
+      //จำนวนเงิน
+      @ApiProperty({
+      description: 'Transaction amount',
+      example: 50,
+      })
+      @IsNumber()
+      @IsPositive()
+      @IsNotEmpty()
+      amount!: number;
+
+      //วันที่จ่าย (optional)
+      @ApiProperty({
+      description: 'Transaction date',
+      example: '2024-01-17T12:00:00Z',
+      })
+      @IsDateString()
+      payDate?: string;
+
+      //วิธีจ่าย (optional)
+      @ApiProperty({
+      description: 'Transaction method',
+      example: PaymentMethod.PROMPTPAY,
+      enum: PaymentMethod,
+      })
+      @IsString()
+      @IsEnum(PaymentMethod, {
+      message:
+            'Method must be either Visa, MasterCard, QR Payment, PromptPay or Mobile Banking',
+      })
+      method?: string;
+
+      //userId เจ้าของ transaction
+      @ApiProperty({
+      description: 'User ID associated with this transaction',
+      type: String,
+      example: '64fa1234567890abcdef1234',
+      })
+      @IsString()
+      @IsNotEmpty()
+      userId!: string; 
+}
+
+export class UpdateTransactionDto {}
+
+
