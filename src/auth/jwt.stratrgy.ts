2774b1409189b33c07495fdf59677a5fe413aa13
nickytk <nccky.ades@gmail.com>
2025-09-05 20:15:52 +0700

    auth stratrgy

--- src/auth/jwt.stratrgy.ts
+++ src/auth/jwt.stratrgy.ts
@@ -7,15 +7,13 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // ดึง token จาก header
-      ignoreExpiration: false, // ให้ตรวจสอบ expire ด้วย
-      secretOrKey: process.env.JWT_SECRET || 'mysecret', // key ที่ใช้เซ็น token
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET || 'mysecret', 
     });
   }
 
   async validate(payload: any) {
-    // payload = ข้อมูลใน token เช่น { sub: userId, email: '...' }
     return { userId: payload.sub, email: payload.email };
-    // ค่านี้จะถูกผูกกับ request.user ใน controller
   }
 }
