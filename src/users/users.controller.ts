4c5be826cb7e4c5814e5639f39efee5b184176c5
nickytk <nccky.ades@gmail.com>
2025-09-12 20:33:06 +0700

    Auth-guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,19 +3,20 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/role.guard';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UsersService } from './users.service';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
 @Controller('users')
 export class UsersController {
     constructor(private readonly userservice: UsersService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get('profile')
-  async getProfile(@Request() req) {
-    const user = await this.userservice.findByEmail(req.user.email);
+  @UseGuards(JwtAuthGuard)
+  getProfile(@CurrentUser() user) {
     return {
-      id : user._id,
+      id: user.userId,
       email: user.email,
-      userName: user.userName,
+      userName: user.username,
+      role: user.role, // เพิ่ม role ได้ด้วย
     };
-}
+  }
 
 }
